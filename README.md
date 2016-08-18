d199b683b46673ef569a7fd138ac802ab9a650a6
Shenzhiweilou <shenzhiweilou@gmail.com>
2016-08-18 10:57:09 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# android_vendor_huawei_h30_u10
-This is a vendor for huawei 3Cw (H30_U10) which is based on MT6582 SoC. 
+Vendor blobs for Huawei h30_u10
+---------------
+
+已修复GPS和录屏等
+heyuhui分享
+
+
+* Compilation
+
+        # repo init -u git://github.com/ferhung-mtk/android.git -b cm-13.0
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_h30_u10-userdebug
+
+---------------
---
+++ h30_u10-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_u10/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_u10/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_u10/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_u10/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_u10/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_u10/proprietary/priv-app,system/priv-app)
